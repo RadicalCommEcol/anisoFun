72f78e2bff8a7bf363955f1c980b7f413dcd882c
Violeta Calleja Solanas <33896005+violetavivi@users.noreply.github.com>
2024-11-19 12:11:16 +0100

    Update obtain_densities.R

--- R/obtain_densities.R
+++ R/obtain_densities.R
@@ -1,25 +1,27 @@
 #' Density of the species at a given intrinsic growth rates vector inside the feasibility domain
 #'
-#' Given
+#' Given a vector r_norm2 in the feasibility domain (so norm2(r_norm2) = 1), computes the densities of the species at equilibrium
 #'
-#' @param center an S vector that contains an intrinsic growth rate vector 
+#' @param r_norm2 an S vector that contains an intrinsic growth rate vector with sqrt(sum(r_norm2*r_norm2)) = 1
 #' @param A_int SxS interaction matrix, where S is the number of species.
 #'
 #' @return A matrix with 1 row and S columns. Each column contains the density for each species
 #'
 #' @examples
 #' A_int <- -1*diag(c(3,5,7))
-#' center <- matrix(c(1,1,1)/sqrt(3),ncol = 1)
-#' obtain_densities(center, A_int) #output should be the (1/3,1/3,1/3)
+#' r_norm2 <- matrix(c(1,1,1)/sqrt(3),ncol = 1)
+#' obtain_densities(r_norm2, A_int) #output should be the (1/3,1/3,1/3)
 #'
 #' @export
 
-obtain_densities <- function(center, A_int){
+library(quadprog)
+
+obtain_densities <- function(r_norm2, A_int){
   
   test_result_A_int <- check_A_int(A_int)
-  test_result_center <- check_center(center, A_int)
+  test_result_r_norm2 <- check_r_norm2(r_norm2, A_int)
   
-  if(test_result_A_int[[1]]==T & test_result_center[[1]]==T){
+  if(test_result_A_int[[1]]==T & test_result_r_norm2[[1]]==T){
     
     if(test_result_A_int[[2]] == "Adding species labels to interaction matrix."){
       
@@ -30,9 +32,9 @@ obtain_densities <- function(center, A_int){
       
     }
     
-    if(test_result_center[[2]] == "Turning the initial growth rates into a unit vector."){
+    if(test_result_r_norm2[[2]] == "Turning the initial growth rates into a unit vector."){
       
-      center <- center/sqrt(sum(center*center))
+      r_norm2 <- r_norm2/sqrt(sum(r_norm2*r_norm2))
       
       cat(test_result_A_int[[2]],"\n")
       
@@ -40,30 +42,21 @@ obtain_densities <- function(center, A_int){
 
   } else {
     
-    if(test_result_center[[2]] == "The vector that contains the initial growth rates should be in the feasibility domain.")){
+    if(test_result_r_norm2[[2]] == "The vector that contains the initial growth rates should be in the feasibility domain.")){
       error_messages <- paste0(test_result_A_int[[2]],"\n",
-                               test_result_center[[2]],"\n")
+                               test_result_r_norm2[[2]],"\n")
       
     }
     cat(error_messages)
   }
+      
+  # Using quadratic programming
+  G <- t(rbind(-A_int,diag(length(r_norm2))))
+  h <- matrix(c(r_norm2,rep(0,length(r_norm2))))
+  sol <- solve.QP(-t(A_int+t(A_int)), r_norm2, G, bvec = h)
+  N_star <- round(sol$solution, digits=12)
   
-dimensions <- ncol(A_int)    
-num_species <- 1:dimensions
-N_star <- matrix(0,nrow = 1, ncol = dimensions)
-sp_names <- paste0("N_", num_species)
-colnames(N_star) <- sp_names
-    
-for(i in 1:dimensions){
-  #Turning each column of A into a unit vector
-  sup_A_col_i <- A_int[,i]
-  sup_A_col_i <- sup_A_col_i / sqrt(sum(sup_A_col_i*sup_A_col_i))
-  N_star[i] <- - center[i] / sup_A_col_i[i]
-  
-
-  }
-
-  #project N_star to the simplex
+  #Project N_star to the simplex
   N_star <- N_star / sum(N_star)
 
   return(N_star)
